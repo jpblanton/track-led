0a9fe02b25589c6f0ee3d9e065b5b27d198b6da8
James Blanton <jamespblanton@gmail.com>
2021-09-14 09:45:05 -0400

    basic button press and mqtt

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify, request
+from flask_mqtt import Mqtt
 
 app = Flask(__name__)
+app.config['MQTT_BROKER_URL'] = '192.168.1.55'  # use the free broker from HIVEMQ
+app.config['MQTT_BROKER_PORT'] = 1883  # default port for non-tls connection
+mqtt = Mqtt(app)
 
-@app.route("/")
+@app.route('/', methods=['POST', 'GET'])
 def index():
-        return "Hello World!"
+
+    return render_template('index.html')
+
+@app.route('/button', methods=['POST'])
+def test():
+
+    if request.method == 'POST':
+        mqtt.publish('test/track/led', 1)
+        return "yes it works"
+    else:
+        return "else"
